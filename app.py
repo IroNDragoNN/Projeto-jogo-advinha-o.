930000a3018966eb3e5d58745e674373c681b86c
IroNDragoNN <andredepaulalima12@gmail.com>
2024-04-25 19:07:28 -0300

    Versao 2.0 - Atualizado a leitura de erros e excessões e foi inserido uma mensagem amigável de encerramento do programa.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Este é o meu primeiro projeto.
 # Jogo de advinhação.
 
+import os
 from random import randint
 from time import sleep
 # Computador pensar...
@@ -14,26 +15,37 @@ palpites = 0
 vitorias = 0
 
 while True:
-    
-    jogador = int(input('Digite um número entre 0 e 10 para tentar a sorte contra o computador: '))
-    if jogador == computador:
-        computador = randint(0, 10)
-        print('Parabéns, você é um oponente formidável.')
-        palpites += 1        
-        vitorias += 1
-        resposta = str(input('Gostaria de continuar tentando ?'))
-        if resposta in 'NAOnao':
-            break
-    elif jogador < computador:
-        print('Você errou! Tente um número maior, talvez acerte numa próxima, hehehe.')
-        palpites += 1
-    elif jogador > computador:
-        print('Você errou! Tente um número menor, talvez acerte numa próxima, hehehe.')
-        palpites += 1
-    elif jogador > 10:
-        print('Número inválido! Tente números entre 0 e 10.')
-    else:
-        print('Jogada inválida. Digite apenas números e que estejam entre 0 e 10')
+    try:
+        jogador = int(input('Digite um número entre 0 e 10 para tentar a sorte contra o computador: '))
+        if jogador == computador:
+            computador = randint(0, 10)
+            print('Parabéns, você é um oponente formidável.')
+            palpites += 1        
+            vitorias += 1
+            resposta = str(input('Gostaria de continuar tentando ?'))
+            if resposta in 'NAOnao':
+                break
+        elif jogador < computador:
+            print('Você errou! Tente um número maior, talvez acerte numa próxima, hehehe.')
+            palpites += 1
+        elif jogador > computador:
+            print('Você errou! Tente um número menor, talvez acerte numa próxima, hehehe.')
+            palpites += 1
+        elif jogador > 10:
+            print('Número inválido! Tente números entre 0 e 10.')
+        else:
+            print('Jogada inválida. Digite apenas números e que estejam entre 0 e 10')
+    except ValueError as erro:
+        os.system('cls')
+        print('\n================================================')
+        print('Você tentou inserir algo diferente de números...')
+        print('Digite apenas números e que estejam entre 0 e 10.')        
+        print('================================================\n')
+        sleep(3)
         
+
+print('\n================================================')
 print(f'Foi um prazer jogar com você.')
 print(f'Ao final, finalizou com {palpites} palpites e {vitorias} vitórias.')
+print('================================================')
+print('PROGRAMA FINALIZADO COM SUCESSO !\n3')
